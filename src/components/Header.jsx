49d39cd0904f2538e305a95dfd9c366174200656
sansan63kc <reagent-163-rw-08@yandex.ru>
2021-01-05 15:55:30 +0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = ()=>{
+    return(
+        <header className="app-header">
+          <img src="http://www.kc-camapa.ru/img/logo/0.png" alt=""/>
+      </header>
+    )
+}
+
+export default Header;
---
+++ src/components/ProfilePage.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const ProfilePage = ()=>{
+    return(
+      <div className="app-content">
+        <div>
+          <img src='https://sun9-32.userapi.com/impg/hd3duKApiU6H5qZe57Z-LLWGRlr8eKZx7TT6WA/pv6tgHvVSbc.jpg?size=2560x1920&quality=96&proxy=1&sign=a30bdb8ecbc75dcb8f48e3120714d173&type=album' alt=""/>
+        </div>
+        
+        <div>
+          ava+descr
+        </div>
+
+        <div>
+          My posts
+          <div>
+            New posts
+          </div>
+
+          <div>
+            <div>post1</div>
+            <div>post2</div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default ProfilePage;
