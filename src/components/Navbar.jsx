35195021ccdcbef41d745e60731768258d24de69
sansan63kc <reagent-163-rw-08@yandex.ru>
2021-01-06 14:20:07 +0400

    css modules

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,20 @@ import React from 'react'
 const Navbar = ()=>{
     return(
       <nav className="app-nav">
-        <div>
+        <div className="navbar__item">
           <a>Profile</a>
         </div>
-        <div>
+        <div className="navbar__item">
           <a>Messages</a>
         </div>
-        <div>
+        <div className="navbar__item">
           <a>News</a>
         </div>
-        <div>
+        <div className="navbar__item">
           <a>Music</a>
         </div>
         <div>
-          <a>Settings</a>
+          <a className="navbar__item">Settings</a>
         </div>
       </nav>
     )
