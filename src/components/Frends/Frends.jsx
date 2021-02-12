02f255c4d5345800482e5f004f8bda0aff988a4a
sansan63kc <reagent-163-rw-08@yandex.ru>
2021-02-12 21:45:26 +0400

    переписал друзей на классовую компоненту

--- src/components/Frends/Frends.jsx
+++ src/components/Frends/Frends.jsx
@@ -1,60 +1,66 @@
-import * as axios from "axios";
 import React from "react";
 import classes from "./Frends.module.css";
+import * as axios from "axios";
 
-let Frends = (props) => {
-  if (props.users.length === 0) {
+class Frends extends React.Component{
 
-    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>{
-      props.setUsers(response.data.items);
-    })
+  constructor(props){
+    super(props)
+
+      /* axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>{
+        props.setUsers(response.data.items); -- пример*/
+      axios.get("http://localhost:3000/items").then(response=>{
+        this.props.setUsers(response.data);
+      }) 
   }
 
-  return (
-    <div className={classes.frendsPage}>
-      <div className={classes.usersList}>
-        {props.users.map((u) => (
-          <div className={classes.userCard} key={u.id}>
-            <div className={classes.userAvatar}>
-              <img className={classes.userAvatarPhoto} src={u.photos.small !=null ? u.photos.small:"https://vk.com/images/camera_200.png" } />  {/* src={u.photoUrl} //-  так в моём json лежит */} 
-            </div>
-            <div className={classes.userInfo__block}>
-              <div className={classes.fullName}>{u.name}</div>{/* в моём json лежит u.fullName */}
-              <div className={classes.workedIn__text}>{"u.workedIn"}</div>
-              <div className={classes.locationName}>
-                {"u.location.country + ", " + u.location.city"}
+  render(){
+    return (
+      <div className={classes.frendsPage}>
+        <div className={classes.usersList}>
+          {this.props.users.map((u) => (
+            <div className={classes.userCard} key={u.id}>
+              <div className={classes.userAvatar}>
+                <img className={classes.userAvatarPhoto} src={u.photoUrl} />  {/* src={u.photoUrl} //-  так в моём json лежит */} {/* src={u.photos.small !=null ? u.photos.small:"https://vk.com/images/camera_200.png" } */}
+              </div>
+              <div className={classes.userInfo__block}>
+                <div className={classes.fullName}>{u.fullName}</div>{/* в моём json лежит u.fullName */}
+                <div className={classes.workedIn__text}>{u.workedIn}</div>
+                <div className={classes.locationName}>
+                  {u.location.country + ", " + u.location.city}
+                </div>
+              </div>
+              <div className={classes.follow__block}>
+                {u.followed ? (
+                  <button
+                    className={classes.follow__btn}
+                    onClick={() => {
+                      this.props.unfollow(u.id);
+                    }}
+                  >
+                    Подписан
+                  </button>
+                ) : (
+                  <button
+                    className={classes.follow__btn}
+                    onClick={() => {
+                      this.props.follow(u.id);
+                    }}
+                  >
+                    Отписан
+                  </button>
+                )}
               </div>
             </div>
-            <div className={classes.follow__block}>
-              {u.followed ? (
-                <button
-                  className={classes.follow__btn}
-                  onClick={() => {
-                    props.unfollow(u.id);
-                  }}
-                >
-                  Подписан
-                </button>
-              ) : (
-                <button
-                  className={classes.follow__btn}
-                  onClick={() => {
-                    props.follow(u.id);
-                  }}
-                >
-                  Отписан
-                </button>
-              )}
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <div className={classes.rightPanel}>
+          <div className={classes.firstPanel}>Там, где куча всяких параметров по поиску друзей</div>
+          <div className={classes.secondPanel}>Там, где возможные друзья</div>
+        </div>
       </div>
-      <div className={classes.rightPanel}>
-        <div className={classes.firstPanel}>Там, где куча всяких параметров по поиску друзей</div>
-        <div className={classes.secondPanel}>Там, где возможные друзья</div>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Frends;
